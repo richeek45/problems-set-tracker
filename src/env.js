11637f5c5cc2bd71e958f916b4d4f9ca65ec3aad
richeek45 <richeek45@gmail.com>
2024-04-28 19:32:10 +0530

    added prisma client and env.js for env validation

---
+++ src/env.js
@@ -0,0 +1,40 @@
+import { z } from "zod";
+import { createEnv } from "@t3-oss/env-nextjs";
+
+export const env = createEnv({
+  server: {
+    DATABASE_URL: z
+      .string()
+      .url()
+      .refine(
+        (str) => !str.includes("YOUR_MYSQL_URL_HERE"),
+        "You forgot to change the default URL"
+      ),
+    NODE_ENV: z
+    .enum(["development", "test", "production"])
+    .default("development"),
+    AUTH_SECRET: process.env.NODE_ENV === "production"
+        ? z.string()
+        : z.string().optional(),
+    AUTH_GITHUB_ID: z.string(),
+    AUTH_GITHUB_SECRET: z.string()
+  },
+
+  client: {
+
+  },
+
+  /**
+   * Run `build` or `dev` with `SKIP_ENV_VALIDATION` to skip env validation. This is especially
+   * useful for Docker builds.
+   */
+  skipValidation: !!process.env.SKIP_ENV_VALIDATION,
+
+  /**
+   * Makes it so that empty strings are treated as undefined. `SOME_VAR: z.string()` and
+   * `SOME_VAR=''` will throw an error.
+   */
+  emptyStringAsUndefined: true,
+
+});
+
