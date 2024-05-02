7445d2d7e28eefa716ba6eb87947916eacae8841
richeek45 <richeek45@gmail.com>
2024-05-02 18:59:36 +0530

    adding form validation while creating new problem

--- src/server/api/routers/problemRouter.ts
+++ src/server/api/routers/problemRouter.ts
@@ -1,27 +1,39 @@
 import { z } from "zod"; 
 import { createTRPCRouter, publicProcedure } from "../trpc";
+import { Difficulty, Status } from "@prisma/client";
+
+const formSchema = z.object({
+  title: z.string().min(5, {
+    message: "Title must be 5 characters long"
+  }),
+  url: z.string({
+    required_error: "Title is required"
+  }).url({
+    message: "Not a valid url"
+  }).min(5, {
+    message: "Url must be 10 characters long"
+  }),
+  status: z.enum([Status.TODO, Status.INPROGRESS, Status.COMPLETED, Status.REPEAT]),
+  difficulty: z.enum([Difficulty.EASY, Difficulty.MEDIUM, Difficulty.HARD]),
+  attempts: z.number(),
+  tags: z.array(z.string()),
+  favourites: z.boolean()
+
+})
 
 export const problemRouter = createTRPCRouter({
-  hello: publicProcedure
-  .query(() => {
-    return {
-      greeting: `Hello`,
-    };
-  }),
-
 
   getAllProblems: publicProcedure.query(async ({ ctx }) => {
     const problems = await ctx.db.problem.findMany();
     return problems;
   }),
   createOneProblem: publicProcedure
-  .input(z.object({ title: z.string(), url: z.string()}))
+  .input(formSchema)
   .mutation(async ({ ctx, input }) => {
-    console.log("....input....")
-    const { title, url } = input;
+    const { title, url, difficulty, status, tags, attempts, favourites } = input;
     const problem = await ctx.db.problem.create({
       data: {
-        title, url, favourites: false, status: "TODO",attempts: 0, difficulty: "EASY", tags: ["array", "tree"] 
+        title, url, favourites, status, tags, attempts, difficulty
       }});
     console.log(problem);
     return problem;
