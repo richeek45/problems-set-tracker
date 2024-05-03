d012377977206371a4533974b53cf1ee756f9b06
richeek45 <richeek45@gmail.com>
2024-05-03 23:58:23 +0530

    added attempts field in filtering and added custom filter function, moved create problem form into a separate file

--- src/components/columnFieldSelection.tsx
+++ src/components/columnFieldSelection.tsx
@@ -50,6 +50,12 @@ export function ColumnFieldSelection({
         >
           Difficulty
         </DropdownMenuCheckboxItem>
+        <DropdownMenuCheckboxItem
+          checked={columnFilterSelection === "attempts"}
+          onCheckedChange={() => setColumnFilterSelection("attempts")}
+        >
+          Attempts
+        </DropdownMenuCheckboxItem>
       </DropdownMenuContent>
     </DropdownMenu>
   )
