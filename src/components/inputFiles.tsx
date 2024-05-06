4bd7b54f0fc317ff16e2efde43b6245891fcfe16
richeek45 <richeek45@gmail.com>
2024-05-06 16:26:57 +0530

    added toaster message for saving, modified row structure from the sheet to save in database, set difficulty level manually

--- src/components/inputFiles.tsx
+++ src/components/inputFiles.tsx
@@ -2,9 +2,11 @@
 
 import { Input } from "~/components/ui/input"
 import { Label } from "~/components/ui/label"
-
+import { inRange } from "lodash";
 import { read, utils } from "xlsx";
-import { ChangeEvent } from "react";
+import { ChangeEvent, useState } from "react";
+import { Difficulty, Status } from ".prisma/client";
+import { ConfirmUploadDialog } from "./confirmUploadDialog";
 
 interface FileInputEvent extends Event {
   target: HTMLInputElement & {
@@ -14,24 +16,34 @@ interface FileInputEvent extends Event {
 
 interface ColType { 
   title: string,
-  tag: string, 
-  link: string, 
-  address: string 
+  tags: string[], 
+  url: string, 
+  // address: string 
 }
 
-interface RowType extends ColType {
-  worksheet: string,
-  problem_number: number,
+export interface RowType extends ColType {
+  status: Status, 
+  problem_number: number, 
+  difficulty: Difficulty,
+  attempts: number,
+  favourites: boolean 
+}
+
+const fixedRowProperties = { 
+  status: Status.TODO, 
+  attempts: 0,
+  favourites: false 
 }
 
 export function InputFile() {
+  const [rows, setRows] = useState<RowType[]>([]);
 
   const fileReader = async (e: React.ChangeEvent extends FileInputEvent ? FileInputEvent: ChangeEvent<HTMLInputElement>) => {
     try{
       const workbook = read(await e.target.files![0].arrayBuffer(), { });
       console.log(workbook)
 
-      const rows: RowType[] = [];
+      let rows: RowType[] = [];
 
       workbook.SheetNames.forEach(sheetName => {
         const worksheet = workbook.Sheets[sheetName];
@@ -52,25 +64,71 @@ export function InputFile() {
 
             if (!cell.l) {
               if (address.includes('A')) {
-                cols.tag = cell.v;
+                cols.tags = [cell.v];
               }
               continue;
             }
 
-            const link = worksheet[address].l;
+            const url = worksheet[address].l;
 
             cols.title = cell.v;
-            cols.address = address;
-            cols.link = link.Target;
+            // cols.address = address;
+            cols.url = url.Target;
           }
 
-          rows.push({worksheet: sheetName, problem_number: row - 5,  ...cols });
+          if (Object.keys(cols).length) {
+            rows.push({ problem_number: row - 5, ...fixedRowProperties, difficulty: Difficulty.EASY,  ...cols });
+          }
         }
       })
 
-      console.log(rows);
+      let problemByTags = new Map(); 
+      rows.forEach(row => {
+        const tags = row.tags[0];
+        const tagNumber = problemByTags.get(tags);
+        if (tagNumber) {
+          problemByTags.set(tags, tagNumber + 1);
+        } else {
+          problemByTags.set(tags, 1);
+        }
+      })
 
-      /// run the query command for saving to the database ......
+      let tagCount = 0; // reset tagCount after every new tags encountered... 
+      let currentTag = '';
+
+      // add 40% of one tags = EASY, 40-80% MEDIUM, 80-100% HARD
+      rows = rows.map(row => {
+        const tags = row.tags[0];
+
+        // tags has changed while iterating
+        if (tags !== currentTag) {
+          tagCount = 0;
+        }
+
+        currentTag = tags;
+        const tagAmount = problemByTags.get(tags); // 36 -> Array
+
+        if (inRange(tagCount, tagAmount * 0.4)) {
+          tagCount ++;
+          return { ...row, status: Status.TODO, difficulty: Difficulty.EASY }
+        } 
+
+        if (inRange(tagCount, tagAmount * 0.4, tagAmount * 0.8)) {
+          tagCount ++;
+          return { ...row, difficulty: Difficulty.MEDIUM }
+        } 
+
+        if (inRange(tagCount, tagAmount * 0.8, tagAmount)) {
+          tagCount ++;
+          return { ...row, difficulty: Difficulty.HARD }
+        } 
+
+        return { ...row, difficulty: Difficulty.EASY };
+      })
+
+      console.log(rows, problemByTags);
+      // find way to import different file formats
+    setRows(rows);
 
     } catch(error){
       console.log(error);
@@ -86,6 +144,7 @@ export function InputFile() {
         onChange={(e) => fileReader(e)}
         accept=".ods, .csv, .xlsx, application/vnd.openxmlformats-officedocument.spreadsheetml.sheet, application/vnd.ms-excel" 
       />
+      <ConfirmUploadDialog rows={rows} />
     </div>
   )
 }
